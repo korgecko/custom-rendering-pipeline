ce09dac921c6a0ce6d44cfdec80d52dbfbd4e01b
Jay <seojoongwon2020@gmail.com>
2025-05-20 10:24:40 +0900

    WIP: vector_utils.h 의 vector 연산부분 3가지 방식으로 구현 가능함을 확인. Todo: Dot, Corss 이해하며 구현하기

--- vector_utils.h
+++ vector_utils.h
@@ -1,43 +1,104 @@
 #pragma once
 #include <cmath>
 
-// 3d 벡터 구조체
-struct Vector3
-{
-    float x;
-    float y;
-    float z;
+struct Vector3 {
+    float x, y, z;
 
-    // 생성자
-    Vector3() : x(0.f), y(0.f), z(0.f) {}
-    Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+    // 1) 값을 안 주고 만들면, 모두 0 으로 채우기
+    Vector3() {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+    }
+
+    // 2) 값을 주고 만들면, 그 값으로 초기화
+    Vector3(float a, float b, float c) {
+        x = a;
+        y = b;
+        z = c;
+    }
 
     // 길이(크기) 반환
-    float Length() const 
-    {
+    float length() const {
         return std::sqrt(x*x + y*y + z*z);
     }
 
-    // 길이 제곱 반환 (루트 연산 없이 빠르게 비교 할 때 사용)
-    float SquaredLength() const
-    {
+    // 길이 제곱 반환 (루트 연산 없이 빠르게 비교할 때 사용)
+    float squaredLength() const {
         return x*x + y*y + z*z;
     }
 
-    bool Normalize()
-    {
-        
+    // ============================================
+    // 방식 1: 멤버 함수로 덧셈 구현
+    // 사용: Vector3 c = a.add(b);
+    Vector3 add(const Vector3& other) const {
+        return Vector3(
+            x + other.x,
+            y + other.y,
+            z + other.z
+        );
     }
 
-    Vector3 GetSafeNormal() const
-    {
-
+    // 방식 3-1: 멤버 함수로 operator+ 오버로딩
+    // 사용: Vector3 c = a + b;
+    Vector3 operator+(const Vector3& other) const {
+        return Vector3(
+            x + other.x,
+            y + other.y,
+            z + other.z
+        );
     }
 
-    Vector3 InnerProduct() const {}
+    // 방식 3-2: 자유 함수(friend)로 operator+ 오버로딩
+    // 사용: Vector3 c = a + b;  (위 멤버 버전 대신 이걸 쓰고 싶다면 주석 해제)
+    /*
+    friend Vector3 operator+(const Vector3& a, const Vector3& b) {
+        return Vector3(
+            a.x + b.x,
+            a.y + b.y,
+            a.z + b.z
+        );
+    }
+    */
 
-    Vector3 DotProduct() const {}
+/* 사용 예시:
+    
+    Vector3 a(1,2,3), b(4,5,6);
 
-    Vector3 CrossProduct() const {}
+    // 방식1: 멤버 함수
+    Vector3 c1 = a.add(b);        
+
+    // 방식2: 자유 함수
+    Vector3 c2 = vectorAdd(a, b);
+
+    // 방식3: operator+ (멤버 혹은 friend)
+    Vector3 c3 = a + b;           
+*/
+
+    bool normalize() {
+        float len = length();
+        if (len <= 0.0f) return false;
+        x /= len; y /= len; z /= len;
+        return true;
+    }
+
+    Vector3 normalized() const {
+        Vector3 v(*this);
+        v.normalize();
+        return v;
+    }
+};
+
+Todo
+/* 
+Vector3 GetSafeNormal() const
+{
 
 }
+
+Vector3 InnerProduct() const {}
+
+Vector3 DotProduct() const {}
+
+Vector3 CrossProduct() const {}
+ */
